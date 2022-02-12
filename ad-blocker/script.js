b9dc751ebdfdcc623376760d536c5246a25e71b4
Tarek Hammami <thammami.me@gmail.com>
2022-02-12 20:40:47 +0100

    Add requested changes related to the SyntaxError that may occur

--- ad-blocker/script.js
+++ ad-blocker/script.js
@@ -1,12 +1,6 @@
 const blockAd = () => {
-  // const skipBtn = document.getElementsByClassName("ytp-ad-skip-button")[0];
-  // if (skipBtn) {
-  //   console.log("SKIP AD BUTTON FOUND");
-  //   skipBtn.click();
-  // } else console.log("SKIP AD BUTTON NOT FOUND");
-
   const video = document.getElementsByClassName('video-stream')[0];
-  video?.currentTime = video?.duration;
+  if (video) video.currentTime = video.duration;
   document.getElementsByClassName('ytp-ad-skip-button')[0]?.click();
 };
 
